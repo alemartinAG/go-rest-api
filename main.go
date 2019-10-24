384adfd173f96eae9bd5586f554fd7f2b8e6cf24
alemartinAG <ale.martin96@hotmail.com>
2019-10-24 13:51:35 -0300

    reading json into struct

--- main.go
+++ main.go
@@ -13,122 +13,48 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
-	"strings"
-	"strconv"
+	//"strings"
+	//"strconv"
 
 	"github.com/gorilla/mux"
 )
 
-type event struct {
-	ID          string `json:"ID"`
-	Title       string `json:"Title"`
-	Description string `json:"Description"`
+
+
+type Matrix []struct {
+	Type    string `json:"matrix"`
+	Values 	[][]int `json:"values"`
 }
 
-type allEvents []event
-
-var events = allEvents{
-	{
-		ID:          "1",
-		Title:       "Introduction to Golang",
-		Description: "Come join us for a chance to learn how golang works and get to eventually try it out",
-	},
-}
 
 func homeLink(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Welcome home!")
+	fmt.Fprintf(w, "put your values in /compute!")
 }
 
-func createEvent(w http.ResponseWriter, r *http.Request) {
-	//var newEvent event
+func putValue(w http.ResponseWriter, r *http.Request) {
+
 	reqBody, err := ioutil.ReadAll(r.Body)
 	if err != nil {
 		fmt.Fprintf(w, "Kindly enter data with the event title and description only in order to update")
 	}
-	
+
 	responseString := string(reqBody)
-	data := strings.SplitAfterN(responseString, "=", 2)
 
-	arg := data[0]
-	value := data[1]
+    data := Matrix{}
+    json.Unmarshal([]byte(responseString), &data)
+    
+    for i := 0; i < len(data); i++ {
+    	fmt.Println("Type: ", data[i].Type)
+    }
 
-	fmt.Printf("responseString: %s\n",responseString)
-	fmt.Printf("argument: %s\n", arg)
-	fmt.Printf("value: %s\n", value)
+	fmt.Fprintf(w, "Recibido Cabeza\n")
 
-	if arg != "LOOP="{
-		return
-	}
-
-	fmt.Printf("responseString: %s\n",responseString)
-	fmt.Printf("argument: %s\n", arg)
-	fmt.Printf("value: %s\n", value)
-
-	cantidad, err := strconv.Atoi(value)
-	if err != nil {
-		fmt.Println("Error en los datos recibidos")
-	}
-
-	count := C.int(cantidad)
-	suma := C.loop(count)
-
-	fmt.Fprintf(w, "Suma = %d\n", suma);
-
-}
-
-func getOneEvent(w http.ResponseWriter, r *http.Request) {
-	eventID := mux.Vars(r)["id"]
-
-	for _, singleEvent := range events {
-		if singleEvent.ID == eventID {
-			json.NewEncoder(w).Encode(singleEvent)
-		}
-	}
-}
-
-func getAllEvents(w http.ResponseWriter, r *http.Request) {
-	json.NewEncoder(w).Encode(events)
-}
-
-func updateEvent(w http.ResponseWriter, r *http.Request) {
-	eventID := mux.Vars(r)["id"]
-	var updatedEvent event
-
-	reqBody, err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		fmt.Fprintf(w, "Kindly enter data with the event title and description only in order to update")
-	}
-	json.Unmarshal(reqBody, &updatedEvent)
-
-	for i, singleEvent := range events {
-		if singleEvent.ID == eventID {
-			singleEvent.Title = updatedEvent.Title
-			singleEvent.Description = updatedEvent.Description
-			events = append(events[:i], singleEvent)
-			json.NewEncoder(w).Encode(singleEvent)
-		}
-	}
-}
-
-func deleteEvent(w http.ResponseWriter, r *http.Request) {
-	eventID := mux.Vars(r)["id"]
-
-	for i, singleEvent := range events {
-		if singleEvent.ID == eventID {
-			events = append(events[:i], events[i+1:]...)
-			fmt.Fprintf(w, "The event with ID %v has been deleted successfully", eventID)
-		}
-	}
 }
 
 func main() {
-	//initEvents()
+
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/", homeLink)
-	router.HandleFunc("/event", createEvent).Methods("POST")
-	router.HandleFunc("/events", getAllEvents).Methods("GET")
-	router.HandleFunc("/events/{id}", getOneEvent).Methods("GET")
-	router.HandleFunc("/events/{id}", updateEvent).Methods("PATCH")
-	router.HandleFunc("/events/{id}", deleteEvent).Methods("DELETE")
+	router.HandleFunc("/compute", putValue).Methods("POST")
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
