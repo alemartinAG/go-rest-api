1cd9f4dc3b73bd0a2bb21371e8b0c4f77e82d593
Julian <altunajulian@gmail.com>
2019-10-31 09:49:31 -0300

    Added comments

--- main.go
+++ main.go
@@ -18,10 +18,13 @@ import (
 	"github.com/gorilla/mux"
 )
 
+//OK value is returned on execution success
 const OK = "SUCCESS"
+
+//ER value is returned on execution failure
 const ER = "ERROR"
 
-
+//Recieves JSON with matrices and sends it to C programm
 func recvMatrices(w http.ResponseWriter, r *http.Request) {
 
 	reqBody, err := ioutil.ReadAll(r.Body)
@@ -31,19 +34,20 @@ func recvMatrices(w http.ResponseWriter, r *http.Request) {
 
 	responseString := string(reqBody)
 
-    str := C.CString(responseString)
+	str := C.CString(responseString)
 	success := C.pass_json(str)
 
 	C.free(unsafe.Pointer(str))
 
+	//TODO: For testing purposes. Remove once the cuda code is working
 	vector := [4]bool{true, false, true, false}
 
 	vectorJson, err := json.Marshal(vector)
-    if err != nil {
-        log.Fatal("Cannot encode to JSON ", err)
-    }
+	if err != nil {
+		log.Fatal("Cannot encode to JSON ", err)
+	}
 
-	if success{
+	if success {
 		C.printMatrices()
 		fmt.Fprintf(w, "{\"status\":\"%s\", \"vector\":%s}", OK, vectorJson)
 	} else {
@@ -51,26 +55,28 @@ func recvMatrices(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+//Recieves transition number, sends it to C and returns enabledVector
 func fireTransition(w http.ResponseWriter, r *http.Request) {
+	//TODO: Implement C calls. Must fire transition and calculate the enabled transitions.
 
+	//TODO: For testing purposes. Remove once the cuda code is working
 	vector := [4]bool{true, false, true, false}
 
 	vectorJson, err := json.Marshal(vector)
-    if err != nil {
-        log.Fatal("Cannot encode to JSON ", err)
-    }
+	if err != nil {
+		log.Fatal("Cannot encode to JSON ", err)
+	}
 
 	fmt.Fprintf(w, "{\"status\":\"%s\", \"vector\":%v}", OK, vectorJson)
-		
+
 }
 
 func main() {
 
 	router := mux.NewRouter().StrictSlash(true)
-	
+
 	router.HandleFunc("/matrices", recvMatrices).Methods("POST")
 	router.HandleFunc("/fire", fireTransition).Methods("POST")
 
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
-
